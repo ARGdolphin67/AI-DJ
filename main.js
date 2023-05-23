709ba444e50f17fbf5b67edc69e7be280cedb338
ARGdolphin67 <109750556+ARGdolphin67@users.noreply.github.com>
2023-05-23 13:33:55 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ leftWristX=0;
 leftWristY=0;
 rightWristX=0;
 rightWristY=0;
+scoreLeftWrist=0;
 
 function preload(){
     song=loadSound("music.mp3");
@@ -23,6 +24,18 @@ function modelLoaded(){
 
 function draw(){
     image(video,0,0,600,500);
+    fill("#FF0000");    
+    stroke("#FF0000");
+
+ if(scoreLeftWrist > 0.2){
+    
+    circle(leftWristX,leftWristY,20);
+    InNumberleftWrist=Number(leftWristY);
+    remove_decimals = floor(InNumberleftWristY);
+    volume = remove_decimals/500;
+    document.getElementById("volume").innerHTML="Volume= " +volume;
+    song.setVolume(volume);
+}
 }
 
 function play(){
@@ -34,6 +47,9 @@ function play(){
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist =" +scoreLeftWrist);
+
         leftWristX=results[0].pose.leftWrist.x;
         leftWristY=results[0].pose.leftWrist.y;
         console.log("leftWristX= " + leftWristX +"leftWristY= "+ leftWristY);
